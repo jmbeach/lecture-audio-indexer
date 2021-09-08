05356d608cc9a1a0c141c9c6a9fbadd80d36fbd6
jmbeach <jaredbeachdesign@gmail.com>
2021-09-07 19:05:01 -0500

    attempt at speech to text

--- src/commands/parse.ts
+++ src/commands/parse.ts
@@ -2,7 +2,10 @@ import { Command, flags } from '@oclif/command'
 import { IConfig } from '@oclif/config';
 import { container } from '../hooks/init/init';
 import { TYPES } from '../ioc/types'
-import { ConfigurationManager, ConfigurationProvider } from '../utils/configuration-manager'
+import { ConfigurationManager } from '../utils/configuration-manager'
+import speech from '@google-cloud/speech';
+import { execSync } from 'child_process'
+import * as fs from 'fs';
 
 export default class Parse extends Command {
   configurationManager: ConfigurationManager;
@@ -29,5 +32,18 @@ export default class Parse extends Command {
       console.error('No API key configured. Run "lecture-audio-indexer conf google-api-key <your-api-key>"')
       return;
     }
+
+    const client = new speech.SpeechClient({
+      key: this.configurationManager.getGoogleApiKey()
+    })
+    execSync(`ffmpeg -i ${args.file} ${args.file}.mp3`)
+    const base64 = fs.readFileSync(`${args.file}.mp3`, { encoding: 'base64' })
+    const result = await client.recognize({
+      audio: {
+        content: base64
+      }
+    });
+    console.log(result);
+    fs.rmdirSync(`${args.file}.mp3`)
   }
 }
