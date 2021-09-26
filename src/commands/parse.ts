9a9adfae4ad21bcfc3112a7aca869dd82f189293
jmbeach <jaredbeachdesign@gmail.com>
2021-09-25 23:03:59 -0500

    it's working

--- src/commands/parse.ts
+++ src/commands/parse.ts
@@ -27,6 +27,17 @@ export default class Parse extends Command {
 
   static args = [{ name: 'file' }]
 
+  nanosToTimestamp(seconds: number, nanos: number) {
+    const secondsInMinute = 60;
+    const nanosInSecond = 1000000000.0;
+    const minutes = Math.floor(seconds / secondsInMinute);
+    const minutesString = minutes < 10 ? `0${minutes}` : minutes.toString();
+    seconds = seconds - (minutes * secondsInMinute);
+    const secondsString = seconds < 10 ? `0${seconds}` : seconds
+    const nanoString = Math.floor((nanos / nanosInSecond) * 1000)
+    return `${minutesString}:${secondsString}.${nanoString}`
+  }
+
   async run() {
     const { args, flags } = this.parse(Parse)
     if (!this.configurationManager.getGoogleApiKey()) {
@@ -43,23 +54,50 @@ export default class Parse extends Command {
     })
     // execSync(`ffmpeg -i "${args.file}" -f s16le -c:a pcm_s16le "${args.file}.raw"`)
     // const base64 = fs.readFileSync(`${args.file}.raw`, { encoding: 'base64' })
+    const name = '6.1.1 IP Addressing.mp4.flac'
     const [operation] = await client.longRunningRecognize({
       audio: {
         // content: base64
-        uri: 'gs://jared-audio-indexer/2.1 Overview of the Week.mp4.raw'
+        uri: `gs://jared-audio-indexer/${name}`
       },
       config: {
         languageCode: 'en-US',
-        encoding: 'LINEAR16',
-        sampleRateHertz: 16000
+        enableWordTimeOffsets: true,
+        enableAutomaticPunctuation: true,
+        encoding: 'FLAC'
       }
     });
-    const result = await operation.promise();
+    const [response] = await operation.promise();
+    const transcriptions = response.results
+      .map(result => result.alternatives[0].transcript);
+    let vttChunks = []
+    for (let i = 0; i < transcriptions.length; i++) {
+      const transcription = transcriptions[i];
+      const words = response.results[i].alternatives[0].words;
+      const sentences = transcription?.split('.')
+        .map(x => x.split(','))
+        // flatmap
+        .reduce((a, b) => [...b, ...a], [])
+        .map(x => x.split('?'))
+        // flatmap
+        .reduce((a, b) => [...b, ...a], [])
+        .filter(x => x).map(x => x.trim());
+      let wordStart = 0;
+      for (let j = 0; j < sentences?.length; j++) {
+        const sentence = sentences[j];
+        const sentenceWords = sentence.split(' ');
+        const startTimeStamp = this.nanosToTimestamp(words[wordStart].startTime?.seconds, words[wordStart].startTime?.nanos);
+        const endTimeStamp = this.nanosToTimestamp(words[wordStart + sentenceWords.length - 1].endTime?.seconds, words[wordStart + sentenceWords.length - 1].endTime?.nanos);
+        const vttChunk = `${startTimeStamp} --> ${endTimeStamp}
+- ${sentence.split('\n').join('\n- ')}`
+        vttChunks.push(vttChunk);
+        wordStart += sentenceWords.length - 1;
+      }
+    }
     if (!fs.existsSync('./out/')) {
       fs.mkdirSync('./out/');
     }
-    const name = args.file.split('/')[args.file.split('/').length - 1];
-    fs.writeFileSync(`./out/${name.replace('.mp4', '')}.json`, JSON.stringify(result));
-    fs.unlinkSync(`${args.file}.raw`)
+    fs.writeFileSync(`./out/${name}.txt`, transcriptions.join('\n'));
+    fs.writeFileSync(`./out/${name}.vtt`, vttChunks.join('\n\n'));
   }
 }
